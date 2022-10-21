2f1765d5d5cf103702e18d5b341a3ae940f55ff5
BurhanHelloWorld <verified0987@gmail.com>
2022-10-21 12:43:27 +0500

    search filter

--- src/components/Home.js
+++ src/components/Home.js
@@ -13,127 +13,161 @@ const Home = ({ navigation }) => {
     navigation.navigate('Addtask')
   }
 
+  useEffect(() => {
+    get()
+  }, [])
+  
+
+  const [searchdata, setSearchdata] = useState("")
   const [qtask, setQtask] = useState(null)
-  const [getdatabase, setGetdatabase] = useState(null)
+  const [getdatabase, setGetdatabase] = useState([])
+  const [filtereddata, setFiltereddata] = useState([])
 
-  const get = async () => {
-    try {
-
-      // await database().ref('tasks').on("value",(tempdata)=>{   
-      // setGetdatabase(tempdata.val());
-      // console.log('gettttttt==========>', tempdata)
-      await database().ref(`tasks`).child(auth().currentUser.uid).on("value", (tempdata) => {
-
-        let get = []
-
-        tempdata.forEach(child => {
-          const obj = {key: child.key, innerData:child.val()}
-          get.push(obj);
+  const search = (text) => {
+    if (text) {
+      const S = getdatabase.filter((item)=> {
+          const itemData = item.innerData.task
+            ? item.innerData.task.toUpperCase()
+            : ''.toUpperCase();
+          const textdata = text.toUpperCase();
+          return itemData.indexOf(textdata) > -1;
         });
-        setGetdatabase(get)
-        console.log('gettttt====>', getdatabase)
-        // setGetdatabase(tempdata.val());
-        // console.log('gettttttt==========>', getdatabase)
-      })
+        console.log('itemfilter========>',JSON.stringify(S))
+      setFiltereddata(S);
+      setSearchdata(text);
     }
-    catch (error) {
-      console.log(error)
+    else {
+      setFiltereddata(getdatabase);
+      setSearchdata(text);
     }
   }
 
-  const storeQtask = async () => {
+
+const get = async () => {
+  try {
+    // await database().ref('tasks').on("value",(tempdata)=>{   
+    // setGetdatabase(tempdata.val());
+    // console.log('gettttttt==========>', tempdata)
+    await database().ref(`tasks`).child(auth().currentUser.uid).on("value", (tempdata) => {
+
+      let get = []
+
+      tempdata.forEach(child => {
+        const obj = { key: child.key, innerData: child.val() }
+        get.push(obj);
+      });
+      setGetdatabase(get)
+      setFiltereddata(get)
+
+      console.log('gettttt====>', getdatabase)
+      // setGetdatabase(tempdata.val());
+      // console.log('gettttttt==========>', getdatabase)
+    })
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
+
+const storeQtask = async () => {
+  try {
+    // const index = getdatabase.length;
+    // const storetask = await database().ref(`tasks/${index}`)
+    // .set({ task: qtask, key:index })
+
+    const storetask = await database().ref(`tasks`).child(auth().currentUser.uid).push()
+      .set({ task: qtask })
+    console.log(storetask)
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+
+const renderView = ({ item }) => {
+
+  const del = async (k) => {
     try {
-      // const index = getdatabase.length;
-      // const storetask = await database().ref(`tasks/${index}`)
-      // .set({ task: qtask, key:index })
-
-      const storetask = await database().ref(`tasks`).child(auth().currentUser.uid).push()
-        .set({ task: qtask })
-      console.log(storetask)
-    } catch (error) {
-      console.log(error)
+      // const D =
+      console.log('kkkkkkkk==========>>>', k)
+      await database().ref('tasks').child(`${item.key}`).remove();
+      const D = await database().ref(`tasks`).child(auth().currentUser.uid).child(`${k}`).remove();
+      console.log(D);
+    } catch (err) {
+      console.log(err);
     }
   }
+  const edit = () => {
+    navigation.navigate("Edittask", { Items: item })
+  }
 
+  // console.log('===============>', item)
+  if (item !== null) {
+    return (
+      <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
+        <View style={{ backgroundColor: 'green', width: '90%', margin: 5, padding: 5, borderRadius: 15 }}>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ color: 'white', fontWeight: 'bold' }}>{item?.innerData.task}</Text>
+            {/* <Text style={{ color: 'white', fontWeight: 'bold' }}>{item?.key}</Text> */}
 
-  const renderView = ({ item }) => {
-
-    const del = async (k) => {
-      try {
-        // const D =
-        console.log('kkkkkkkk==========>>>', k)
-        await database().ref('tasks').child(`${item.key}`).remove();
-        const D = await database().ref(`tasks`).child(auth().currentUser.uid).child(`${k}`).remove();
-        console.log(D);
-      } catch (err) {
-        console.log(err);
-      }
-    }
-    const edit = () => {
-      navigation.navigate("Edittask", {Items : item})
-    }
-
-    // console.log('===============>', item)
-    if (item !== null) {
-      return (
-        <View style={{ width: '100%', justifyContent: 'center', alignItems: 'center' }}>
-          <View style={{ backgroundColor: 'green', width: '90%', margin: 5, padding: 5, borderRadius: 15 }}>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text style={{ color: 'white', fontWeight: 'bold' }}>{item?.innerData.task}</Text>
-              {/* <Text style={{ color: 'white', fontWeight: 'bold' }}>{item?.key}</Text> */}
-
-              <TouchableOpacity onPress={() => del(item?.key)} style={{ width: 30, margin: 5 }}>
-                <Text> <AntDesign name='delete' style={{ fontSize: 18, color: 'white' }} /> </Text>
-              </TouchableOpacity>
-            </View>
-            <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
-              <Text style={{ color: 'lightgreen' }}>{item?.innerData.date}</Text>
-              <TouchableOpacity onPress={()=>edit()} style={{ width: 30, margin: 5 }}>
-                <Text> <AntDesign name='edit' style={{ fontSize: 18, color: 'white' }} /> </Text>
-              </TouchableOpacity>
-            </View>
+            <TouchableOpacity onPress={() => del(item?.key)} style={{ width: 30, margin: 5 }}>
+              <Text> <AntDesign name='delete' style={{ fontSize: 18, color: 'white' }} /> </Text>
+            </TouchableOpacity>
+          </View>
+          <View style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ color: 'lightgreen' }}>{item?.innerData.date}</Text>
+            <TouchableOpacity onPress={() => edit()} style={{ width: 30, margin: 5 }}>
+              <Text> <AntDesign name='edit' style={{ fontSize: 18, color: 'white' }} /> </Text>
+            </TouchableOpacity>
           </View>
         </View>
-      )
-    }
-  }
-
-  return (
-    <View style={styles.Container}>
-      <Text style={styles.Maintext}>Lists</Text>
-
-      <TouchableOpacity onPress={() => get()}>
-        <Text>GetData</Text>
-      </TouchableOpacity>
-
-
-      <View style={{ flex: 1 }}>
-        <View style={{ backgroundColor: 'darkgreen', height: '85%', width: '100%' }}>
-          <FlatList
-            data={getdatabase}
-            renderItem={renderView}
-          />
-        </View>
-        <View style={styles.Container1}>
-          <TouchableOpacity onPress={() => add()} style={styles.btn}>
-            <MaterialIcons name='add' style={{ fontSize: 40, color: 'darkgreen' }} />
-          </TouchableOpacity>
-        </View>
       </View>
+    )
+  }
+}
 
-      <View style={styles.inputView}>
-        <TextInput style={styles.input}
-          placeholder='Enter Quick Task Here'
-          value={qtask}
-          onChangeText={(txt) => setQtask(txt)}
-          multiline
+return (
+  <View style={styles.Container}>
+    <View style={{flexDirection:'row',margin:5}}>
+      <Text style={styles.Maintext}>Lists</Text>
+      <TextInput style={{borderRadius:10,elevation:2, backgroundColor: 'white', padding: 10, height:35,width:'80%' }}
+        placeholder='Search bar'
+        value={searchdata}
+        onChangeText={(text) => search(text)}
+      />
+    </View>
+    {/* <TouchableOpacity onPress={() => get()}>
+      <Text>GetData</Text>
+    </TouchableOpacity> */}
+
+
+    <View style={{ flex: 1 }}>
+      <View style={{ backgroundColor: 'darkgreen', height: '85%', width: '100%' }}>
+        <FlatList
+          data={filtereddata}
+          renderItem={renderView}
         />
-        <TouchableOpacity onPress={() => storeQtask()} style={styles.btn1}>
-          <Text>Done</Text>
+      </View>
+      <View style={styles.Container1}>
+        <TouchableOpacity onPress={() => add()} style={styles.btn}>
+          <MaterialIcons name='add' style={{ fontSize: 40, color: 'darkgreen' }} />
         </TouchableOpacity>
       </View>
     </View>
-  )
+
+    <View style={styles.inputView}>
+      <TextInput style={styles.input}
+        placeholder='Enter Quick Task Here'
+        value={qtask}
+        onChangeText={(txt) => setQtask(txt)}
+        multiline
+      />
+      <TouchableOpacity onPress={() => storeQtask()} style={styles.btn1}>
+        <Text>Done</Text>
+      </TouchableOpacity>
+    </View>
+  </View>
+)
 }
 
 export default Home
